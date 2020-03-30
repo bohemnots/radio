db274c9f52c4d1c00dffecba55857763055fff8c
hsanoyan <hovosanoyan@gmail.com>
2020-03-31 02:40:51 +0400

    Change path to views and .env config

--- packages/server/src/server.js
+++ packages/server/src/server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
 
-require('dotenv').config({ path: path.join(process.cwd(), '../.env') });
+require('dotenv').config();
 
 const { client, fromInfo, getMetadata } = require('./mongo');
 const live = require('./watch');
@@ -12,7 +12,7 @@ const installStatic = require('./middleware/static');
 
 const port = process.env.PORT || 3000;
 
-app.set('views', path.join(process.cwd(), './views'));
+app.set('views', path.join(process.cwd(), 'src/views'));
 app.set('view engine', 'pug');
 
 const init = async () => {
