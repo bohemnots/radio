72baab351f7da622ff888e404cd516ecdeb299a7
hsanoyan <hovosanoyan@gmail.com>
2020-04-26 00:32:22 +0400

    Load audio after becoming online

--- packages/client/src/hooks/usePlayer.js
+++ packages/client/src/hooks/usePlayer.js
@@ -1,37 +1,50 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import * as config from "../config";
 
-const audio = new Audio(config.streamUrl);
 
 export default function usePlayer() {
+  const audio = React.useRef(new Audio(config.streamUrl));
+
   const [firstTime, setFirstTime] = React.useState(true);
-  const [state, setState] = React.useState(false)
-  const pause = () => audio.pause();
+  const [state, setState] = React.useState(false);
+
+  const pause = useCallback(() => audio.current.pause(), [audio]);
+  const play = useCallback(() => audio.current.play(), [audio]);
+
   const toggle = () => {
-    audio.paused ? play() : pause()
+    audio.current.paused ? play() : pause()
     setState(!state) // to update view
   }
-  const play = React.useCallback(() => {
-    if (!audio.paused) {
+
+  const online = React.useCallback(() => {
+    if (!audio.current.paused) {
       return;
     }
+    audio.current.load();
     window.focus();
-    audio.play().catch(() => {});
+    audio.current.play().catch(() => { });
+  }, []);
+
+  const offline = React.useCallback(() => {
+    setState(false)
   }, []);
 
 
+
+
   React.useEffect(() => {
-    window.addEventListener("online", play);
-    return () => window.removeEventListener("online", play);
+    window.addEventListener("online", online);
+    window.addEventListener("offline", offline);
+    return () => window.removeEventListener("online", online);
   });
 
   React.useEffect(() => {
     if (firstTime) {
-      play();
+      play().catch(console.error)
       setFirstTime(false);
     }
   }, [firstTime, play]);
 
-  return [!audio.paused, toggle];
+  return [!audio.current.paused, toggle];
 }
