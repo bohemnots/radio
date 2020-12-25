4a03f89c3bf75ffb936fd8bc1397f51f2d2d14ce
hos <hovosanoyan@gmail.com>
2020-12-25 04:05:54 +0400

    Define both streams in config

--- packages/client/src/config/index.js
+++ packages/client/src/config/index.js
@@ -2,7 +2,8 @@ const { REACT_APP_HOST } = process.env;
 
 const host = REACT_APP_HOST || "";
 
-export const streamUrl = "https://bhmnts.out.airtime.pro/bhmnts_a";
+export const streamA = "https://bhmnts.out.airtime.pro/bhmnts_a";
+export const streamB = "https://bhmnts.out.airtime.pro/bhmnts_b";
 
 export const metadataUrl = `${host}/metadata`;
 
